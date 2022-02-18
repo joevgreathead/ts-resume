0478be0d573e22def3c623e0951c0b2cfa486796
joevgreathead <joevgreathead@gmail.com>
2022-02-17 20:03:23 -0800

    Add hydrate distinction for reac-snap

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { render } from "react-dom";
+import { hydrate, render } from "react-dom";
 import "./index.css";
 import "normalize.css";
 import { Resume } from "./resume";
 
-render(
-  <React.StrictMode>
-    <Resume />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+if (rootElement?.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <Resume />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <Resume />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
